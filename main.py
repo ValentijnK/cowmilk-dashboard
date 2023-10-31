e8b0586036258dcbc72ffe988a2e1dc249e7a8fe
Jip van Buitenen <jipvanbuitenen@gmail.com>
2023-10-31 15:54:06 +0100

    cleaned code

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ cows = pd.read_csv('number_of_dairy_cows.csv')
 pd.set_option('display.max_columns', None)
 
 milk_production = milk_production[milk_production['dairyprod'].isin(['D1110A'])]
-milk_production = milk_production[milk_production['TIME_PERIOD'].isin([2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2021, 2022])]
 
 # drop columns
 milk_production = milk_production.drop(columns=['DATAFLOW', 'LAST UPDATE', 'freq', 'dairyprod', 'milkitem', 'OBS_FLAG'])
@@ -19,4 +18,4 @@ print("Duplicates in cows:", cows.duplicated().sum())
 
 # Merge the two dataframes
 df = pd.merge(milk_production, cows, on=['country', 'year'])
-df.head(25)
+df.head(20)
