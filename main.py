1cc89712880fd710790790049ef2165966839395
Valentijn Kilian <valentijnkilian@gmail.com>
2023-10-31 15:07:36 +0100

    Added datasets

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello world')
+import pandas as pd
+
+milk_production = pd.read_csv('production_milk_EU.csv')
+cows = pd.read_csv('number_of_dairy_cows.csv')
+pd.set_option('display.max_columns', None)
+
+
+
+print(cows.head(10))
+print(milk_production.head(10))
